b76bcf78347383deaa9ad012f08203364706a238
Tom Settle <tom.k.settle@gmail.com>
2023-02-10 08:50:36 -0600

    Loop repeated 15x

---
+++ Iterations-Mod4-Ex1/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Iterations_Mod4_Ex1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int j = 0; j < 15; j++)
+            {
+                Console.WriteLine($"Iteration #{j+1}");
+                for (int i = 0; i < 5; i++)
+                {
+                    Console.WriteLine($"{i}");
+                }
+            }
+        }
+    }
+}
